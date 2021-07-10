3f243ab430748bb5053b01bfe1b35fe0dcd0102f
evertonvargas <evertonpassa7@gmail.com>
2021-07-10 08:56:23 -0300

    header pronto

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  background: var(--blue);
+` 
+
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 2rem 1rem 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button{
+    font-size: 1rem;
+    color: #fff;
+    background: var(--blue-light);
+    /* button vem com uma border por padrão */
+    border: 0; 
+    padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 2.5rem;
+
+    transition: filter 0.2s;
+    
+    &:hover{
+      filter: brightness(0.9);
+    }
+  }
+`
