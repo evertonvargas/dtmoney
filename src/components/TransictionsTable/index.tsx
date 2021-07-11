89ba8254540b8eb5511fc30a3540df303aa6b9d8
evertonvargas <evertonpassa7@gmail.com>
2021-07-11 09:57:46 -0300

    Usando MirageJs

---
+++ src/components/TransictionsTable/index.tsx
@@ -0,0 +1,51 @@
+import { useEffect } from "react";
+import { Container } from "./styles";
+
+export function TransactionsTable(){
+  useEffect(()=>{
+    fetch('http://localhost:3000/api/transactions')
+    .then(response=>response.json())
+    .then(data=>console.log(data))
+  }, [])
+
+  return(
+    <Container>
+      <table>
+        <thead>
+            <tr>
+              <th>Título</th>
+              <th>Preço</th>
+              <th>Categoria</th>
+              <th>Data</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td>Desenvolvimento de website</td>
+              <td className="deposit">R$ 12000</td>
+              <td>Venda</td>
+              <td>13/04/2021</td>
+            </tr>
+            <tr>
+              <td>Hamburguer</td>
+              <td className="withdraw">-R$ 59,00</td>
+              <td>Alimentação</td>
+              <td>13/04/2021</td>
+            </tr>
+            <tr>
+              <td>Desenvolvimento de website</td>
+              <td className="deposit">R$ 12000</td>
+              <td>Venda</td>
+              <td>13/04/2021</td>
+            </tr>
+            <tr>
+              <td>Hamburguer</td>
+              <td className="withdraw">-R$59,00</td>
+              <td>Alimentação</td>
+              <td>13/04/2021</td>
+            </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
